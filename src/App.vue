ae3bdf9682f5fbe1b61fcedaf5a91fe2248b91eb
PhGe <gerard.philipp@web.de>
2023-11-21 02:52:24 +0100

    ss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcomes to Your Vue.js App"/>
+  <HelloWorld msg="Welcsomes to Your Vue.js App"/>
 </template>
 
 <script>
