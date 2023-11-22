100d6f26bb172f5a3ed24efb0d108209838a47a0
PhGe <gerard.philipp@web.de>
2023-11-22 21:16:47 +0100

    OK

--- tests/welcome.test.js
+++ tests/welcome.test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const PAGE_URL = 'https://phge.github.io/CourtMaster/';
-const checkText = 'Welcomeasdasfa';
+const checkText = 'Welcome';
 const currentDate = new Date().toISOString().replace(/:/g, '-').substring(0, 19);
 
 test('Verify Welcome Text', async ({ page }) => {
