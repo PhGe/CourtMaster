846b010db3464ab4b7539ffa25c0bc3053e731f6
PhGe <PhGe@users.noreply.github.com>
2023-11-22 14:01:39 +0000

    deploy: 616a722c525b03bed36504822d1e0e3c8c1d71bd

--- playwright.config.js
+++
@@ -1,5 +0,0 @@
-const { test } = require('@playwright/test');
-
-module.exports = {
-  testMatch: ["**/tests/**/*.test.js"],
-};
--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: './', // Merge the publicPath configuration here
-  outputDir: 'dist' // Merge other configurations here
-});
