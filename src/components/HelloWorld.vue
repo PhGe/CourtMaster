d260716e5745ec20b065048623328ee418d1fe95
PhGe <gerard.philipp@web.de>
2023-11-22 21:34:02 +0100

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 v-if="showMessage">{{ msg }}</h1>
   </div>
 </template>
 
@@ -9,8 +9,14 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
-}
+  },
+  data() {
+    return{
+
+    showMessage: process.env.NODE_ENV ==='production'
+  };
+}}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
