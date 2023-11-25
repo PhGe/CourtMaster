4eef635780670a948c48634fc05b91d3e337f96f
PhGe <gerard.philipp@web.de>
2023-11-25 16:43:44 +0100

    neu

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: './', // Merge the publicPath configuration here
+  outputDir: 'dist' // Merge other configurations here
+});
