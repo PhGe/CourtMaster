924b7c18a98f83cf1cb26b4ec3b421b704777a31
PhGe <gerard.philipp@web.de>
2023-11-27 00:40:00 +0100

    a

--- tests/database.test.js
+++ tests/database.test.js
@@ -4,41 +4,40 @@ const { Pool } = require('pg');
 const PAGE_URL = 'https://phge.github.io/CourtMaster/';
 const DATABASE_URL = 'postgres://dyjfpsho:a0N5GKX2kyBNAZ68w8Gpaw8AsGShUH6j@flora.db.elephantsql.com/dyjfpsho';
 
-test('Check Database Users', async ({ page }) => {
-   
-    //database connection pool
+test('Check Database User - Philipp Admin', async ({ page }) => {
+    // Database connection pool
     const pool = new Pool({
         connectionString: DATABASE_URL,
     });
 
     try {
-        // Fetch data from the database
-        const result = await pool.query('SELECT * FROM users');
-        const users = result.rows;
-        
+        // Fetch data from the database for the specific user (Philipp - Admin)
+        const result = await pool.query('SELECT * FROM users WHERE username = $1 AND role = $2', ['Philipp', 'Admin']);
+        const user = result.rows[0];
+
         // Go to the page
         await page.goto(PAGE_URL);
 
         // Perform your assertions
-        await expect(users).toBeTruthy();
-        console.log(users)
+        await expect(user).toBeTruthy();
+        console.log(user);
 
-        for (const user of users) {
+        console.log(`Before waitForSelector: ${user.username} - ${user.role}`);
+        console.log(await page.evaluate(() => {
+            console.log('Browser Console Output');
+            return 'Done evaluating';
+        }));
+        
 
-            console.log(`Before waitForSelector: ${user.username} - ${user.role}`);
-            console.log(await page.evaluate(() => console.log('Browser Console Output')));
+        // Wait for the element to be visible
+        await page.waitForSelector(`text=/${user.username}/`, { timeout: 10000 });
 
+        console.log(`After waitForSelector: ${user.username} - ${user.role}`);
 
-            await page.waitForSelector(`text=/${user.username}/`, { timeout: 100000 });
-
-            console.log(`After waitForSelector: ${user.username} - ${user.role}`);
-
-            console.log(await page.innerHTML(`text=/${user.username}/`));
-            await expect(page.locator(`text=/${user.username}/`)).toBeVisible();
-        }
+        console.log(await page.innerHTML(`text=/${user.username}/`));
+        await expect(page.locator(`text=/${user.username}/`)).toBeVisible();
     } finally {
         // Close the database connection pool
         await pool.end();
     }
 });
-
