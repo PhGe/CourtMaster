1e988335637336be818f60eeb2005cce6ee548a6
PhGe <gerard.philipp@web.de>
2023-11-26 21:27:52 +0100

    Add backend to frontend connection and show Database testusers

---
+++ src/components/PlayerNames.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+      <h1>User Lista</h1>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.username }} - {{ user.role }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        users: [], // This is where you store the fetched users
+      };
+    },
+    mounted() {
+      // Make an HTTP request to your backend API
+      fetch('http://localhost:3000/users/v1')
+        .then((response) => response.json())
+        .then((data) => {
+          // Update the users data with the fetched data
+          this.users = data;
+        })
+        .catch((error) => {
+          console.error('Error fetching users:', error);
+        });
+    },
+  };
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li::before {
+    content: '•'; /* Use a bullet point as the content before the li */
+    color: blue;
+    margin-right: 5px; /* Adjust margin if needed */
+
+  }
+  
+  </style>
---
+++ src/views/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <img alt="Vue logo" src="../assets/Tennisball.png">
+    <HelloWorld msg="Welcome Tennis players du looser !!!!!!!"/>
+    <PlayerNames/>
+  </template>
+  
+  <script>
+  import HelloWorld from '../components/HelloWorld.vue';
+  import PlayerNames from '../components/PlayerNames.vue';
+  
+  
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      PlayerNames
+  }
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    
+  }
+  img{
+    width: 150px;
+    height: 150px;
+  }
+  </style>
+  
