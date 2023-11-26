06ba83cbfc8695aafc65addad5e8f77194504773
PhGe <gerard.philipp@web.de>
2023-11-27 00:26:20 +0100

    go

--- tests/database.test.js
+++ tests/database.test.js
@@ -26,12 +26,12 @@ test('Check Database Users', async ({ page }) => {
         for (const user of users) {
             console.log(`Before waitForSelector: ${user.username} - ${user.role}`);
 
-            await page.waitForSelector(`Philipp`);
+            await page.waitForSelector(`text=/${user.username}/`, { timeout: 100000 });
 
             console.log(`After waitForSelector: ${user.username} - ${user.role}`);
 
-            console.log(await page.innerHTML(`Philipp`));
-            await expect(page.locator(`tPhilipp`)).toBeVisible();
+            console.log(await page.innerHTML(`text=/${user.username}/`));
+            await expect(page.locator(`text=/${user.username}/`)).toBeVisible();
         }
     } finally {
         // Close the database connection pool
