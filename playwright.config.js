ce4cb1a1ead71ccf25540cd213d952783cb28d98
PhGe <gerard.philipp@web.de>
2023-11-27 00:30:39 +0100

    test

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,11 @@
-const { defineConfig  } = require('@playwright/test');
+const { defineConfig } = require('@playwright/test');
 
 export default defineConfig({
   retries: process.env.CI ? 2 : 0, // set to 2 when running on CI
   // ...
   use: {
     trace: 'on-first-retry', // record traces on first retry of each test
+    headless: false,
   },
 });
 
