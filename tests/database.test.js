a7450fd9b7f0ba13a04943e7ecb583ab04b69e9f
PhGe <gerard.philipp@web.de>
2023-11-26 22:01:57 +0100

    added database test

---
+++ tests/database.test.js
@@ -0,0 +1,24 @@
+const { test, expect } = require('@playwright/test');
+
+const PAGE_URL = 'https://phge.github.io/CourtMaster/';
+
+test('Check Database Users', async ({ page }) => {
+
+    //fetch backend
+    const userResponse = await fetch('http://localhost:3000/users/v1')
+    const users = await userResponse.json();
+    console.log(users)
+
+    //goto Page
+    await page.goto(PAGE_URL);
+  
+    await expect(users).toBeTruthy();
+    
+    //check if loaded successfully
+    for (const user of users) {
+        await expect(page.locator(`text=${user.username} - ${user.role}`)).toBeVisible();
+    }
+  });
+
+
+
