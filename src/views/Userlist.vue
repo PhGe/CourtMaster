2fbd4cfc07fb8645e7d42a8e08886b37d0232ce5
PhGe <gerard.philipp@web.de>
2023-12-01 14:35:52 +0100

    added route to calendar

--- src/views/Userlist.vue
+++ src/views/Userlist.vue
@@ -4,6 +4,7 @@
       <h1>Login Successfull!</h1>
         <img src="../assets/Tennisball.png">
       <PlayerNames></PlayerNames>
+      <router-link to="/calendar" class="link">Calendar</router-link>
     </div>
   </template>
 
@@ -20,6 +21,27 @@ import PlayerNames from '@/components/PlayerNames.vue';
   img{
     width: 150px;
     height: 150px;
+  }
+
+  .links {
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+  }
+  
+  .link {
+    margin-bottom: 10px;
+    padding: 10px;
+    background-color: #3498db;
+    color: #fff;
+    text-decoration: none;
+    border-radius: 5px;
+    text-align: center;
+    transition: background-color 0.3s;
+  }
+  
+  .link:hover {
+    background-color: #2980b9;
   }
 </style>
   
