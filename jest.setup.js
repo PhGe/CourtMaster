ccd9aeba4de6c8288e8fe819cad4f95521d651cd
PhGe <gerard.philipp@web.de>
2024-02-24 23:52:49 +0100

    test

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
 process.env.NODE_ENV = 'test'; // Set the environment to test
 
 // Optionally set the test port
-process.env.TEST_PORT = 'https://court-master-e4c0d72c16c5.herokuapp.com'; // Set the test port
+process.env.TEST_PORT = '3001'; // Set the test port
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const {
 
 // Create an Express application
 const app = express();
-const port = process.env.PORT || 'https://court-master-e4c0d72c16c5.herokuapp.com';
+const port = process.env.PORT || 3000;
 const testPort = process.env.TEST_PORT || 3001; // Update the default test port
 
 
